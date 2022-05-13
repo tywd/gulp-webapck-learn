9db0f4b1d53fc337f1cd2d521045e7523cef78ec
shichuyu <1042048096@qq.com>
2022-05-13 18:21:37 +0800

    update: babel转移js

--- webpack.config.js
+++ webpack.config.js
@@ -5,17 +5,28 @@ const path = require('path')
 module.exports = {
     mode: 'development',
     devtool: 'eval-cheap-module-source-map',
-    entry: {
-        index: path.resolve(__dirname, './src/js/index.js'),
-    },
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, './dist'),
-    },
+    // entry: {
+    //     index: path.resolve(__dirname, './src/js/index.js'),
+    // },
+    // output: {
+    //     filename: '[name].js',
+    //     path: path.resolve(__dirname, './dist'),
+    // },
     module: {
-        rules: [
-
-        ]
+        rules: [{
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env'],
+                    plugins: ['@babel/plugin-proposal-object-rest-spread',
+                        '@babel/plugin-transform-runtime'
+                    ],
+                    cacheDirectory: true,
+                }
+            }
+        }]
     },
     resolve: {
         alias: {
